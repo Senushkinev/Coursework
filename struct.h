3aa489e1fcd06d9afeda9232c747525adffa6f12
Senushkin <42892719+Senushkinev@users.noreply.github.com>
2018-12-09 20:02:31 +0300

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,5 +1,6 @@
 #ifndef STRUCT
 #define STRUCT
+
 struct Sentence {
 	wchar_t* Sent;
 	size_t sentLenght;
@@ -9,4 +10,5 @@ struct Text {
 	struct Sentence *sentences;
 	size_t buffLenght;
 };
+
 #endif
